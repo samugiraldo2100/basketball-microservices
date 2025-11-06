6038a882f5f0ea431d80ab5736559dd9e5decbac
Samuel Giraldo <segiraldo_274@cue.edu.co>
2025-11-06 10:07:56 -0500

    Update EquipoRequests.java

--- equipos-service/src/main/java/com/example/teams/controller/dto/EquipoRequests.java
+++ equipos-service/src/main/java/com/example/teams/controller/dto/EquipoRequests.java
@@ -12,13 +12,7 @@ public class EquipoRequests {
                         @NotBlank String conference,
                         Integer championships) {}
 }
-package com.example.players.controller.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
-public class JugadorRequests {
-    public record Create(@NotBlank String fullName,
-                         @NotBlank String position,
                          Integer jerseyNumber,
                          Integer equipoId) {}
     public record Update(@NotBlank String fullName,
